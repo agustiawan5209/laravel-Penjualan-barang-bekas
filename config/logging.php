2a0a607ccce35a3f685816a8bec204d1fe947eb9
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-08-09 07:14:46 +0800

    logging and trusproxy

--- config/logging.php
+++ config/logging.php
@@ -54,13 +54,10 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
         ],
-
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'driver' => 'errorlog',
+            'level' => 'debug',
         ],
 
         'daily' => [
