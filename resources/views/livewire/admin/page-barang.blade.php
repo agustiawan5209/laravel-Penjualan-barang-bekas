b4e0d7a26c140b8c2c5e90e99517d065bbfb2244
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-08-23 22:11:11 +0800

    Change Composer

--- resources/views/livewire/admin/page-barang.blade.php
+++ resources/views/livewire/admin/page-barang.blade.php
@@ -337,7 +337,7 @@
             </div>
 
             <div class="flex items-center justify-between">
-                <button wire:click='edit'
+                <button wire:click='EditBarang({{$itemID}})'
                     class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="button">
                     Simpan
