68db3ad28f09f16a7de3591737c6ca3c174589cc
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-01-18 23:21:13 +0800

    update pagetambahfoto.php

--- app/Http/Livewire/Admin/PageTambahFoto.php
+++ app/Http/Livewire/Admin/PageTambahFoto.php
@@ -13,9 +13,10 @@ class PageTambahFoto extends Component
     use WithFileUploads;
     public $barang_id;
     public $foto_barang;
-    public $itemAdd= false;
-    public $default_foto ;
-    public function mount($id){
+    public $itemAdd = false;
+    public $default_foto;
+    public function mount($id)
+    {
         $this->barang_id = $id;
     }
     public function render()
@@ -24,37 +25,40 @@ class PageTambahFoto extends Component
         $fotoBarang = FotoBarang::where('barang_id', $barang->id)->where('jenis', '1')->get();
         // $this->foto_barang = $fotoBarang->foto;
         return view('livewire.admin.page-tambah-foto', [
-            'barang'=> $barang,
-            'fotoBarang'=> $fotoBarang,
+            'barang' => $barang,
+            'fotoBarang' => $fotoBarang,
         ]);
     }
-    public function count(){
+    public function count()
+    {
         $this->itemAdd = true;
     }
     public function tambahFoto()
     {
         $barang = Barang::find($this->barang_id);
+        if ($this->foto_barang !== null) {
 
-        // dd($this->foto_barang, $this->default_foto);
-        $randomize = $barang->nama_produk .'-'. $this->foto_barang->getClientOriginalName();
-        FotoBarang::create([
-            'barang_id'=> $this->barang_id,
-            'foto'=> $randomize,
-            'default'=> 'no',
-            'jenis'=> '1',
-        ]);
+            // dd($this->foto_barang, $this->default_foto);
+            $randomize = $barang->nama_produk . '-' . $this->foto_barang->getClientOriginalName();
+            FotoBarang::create([
+                'barang_id' => $this->barang_id,
+                'foto' => $randomize,
+                'default' => 'no',
+                'jenis' => '1',
+            ]);
 
-        $this->foto_barang->storeAs('upload', $randomize);
+            $this->foto_barang->storeAs('upload', $randomize);
+        }
         $this->itemAdd = false;
         $this->foto_barang = null;
-
     }
     public function hapusFoto($id)
     {
         FotoBarang::find($id)->delete();
     }
-    public function default_foto($id){
-        FotoBarang::find($id)->update(['default'=> 'yes']);
-        FotoBarang::whereNot('id', [$id])->where('jenis', '1')->update(['default'=> 'no']);
+    public function default_foto($id)
+    {
+        FotoBarang::find($id)->update(['default' => 'yes']);
+        FotoBarang::whereNot('id', [$id])->where('jenis', '1')->update(['default' => 'no']);
     }
 }
